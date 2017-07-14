626c471d7cc3c760ae4a350598fe9105ecec5a6e
manyanxp <manyanxp.cat.com@gmail.com>
2017-07-14 19:04:58 +0900

    Ｓｅｒｖｅｒ修正

--- Python/SimATS/SimATS/application.py
+++ Python/SimATS/SimATS/application.py
@@ -3,12 +3,16 @@
 import sys
 import io
 import socket
+import time
 import threading
+import signal
 from packet.common_header import IdentificationUnit
 
 
 class TcpServer:
     def __init__(self, bindip, bindport):
+        """通信ソケットの作成"""
+        self.clients = []
         self.bindip = bindip
         self.bindport = bindport
         self.__server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
@@ -25,23 +29,41 @@ class TcpServer:
     def accept(self):
         """接続要求処理"""
         client,addr = self.__server.accept()
-        print('[*] Accepted connectoin from: %s:%d' % (addr[0],addr[1]))
-        client_handler = threading.Thread(target=self.handle_client,args=(client,))
+        print('[*]Accepted connectoin from: %s:%d' % (addr[0],addr[1]))
+        self.clients.append((client, addr))
+        client_handler = threading.Thread(target=self.handle_client,args=(client,addr),)
         client_handler.daemon = True
         client_handler.start()
 
-    def handle_client(client_socket):
-        """クライアントハンドル"""
+    def remove_conection(self, con, addr):
+        """クライアントと接続を切る"""
+        print('切断{}'.format(addr))
+        con.close()
+        self.clients.remove((con, addr))
 
-        print('受信処理開始{0:s}'.format(client_socket.bindip))
+    def send_packet(self, messege):
+        """クライアントへのパケット送信"""
+        for c in self.clients:
+            c[0].sendto(message, c[1])
+
+    def handle_client(self, con, addr):
+        """クライアントハンドル"""
 
         while True:
             bufsize=1024
-            request = client_socket.recv(bufsize)
-            do_message_dipatch(client_socket, request)
-    
-        print('受信処理終了{0:s}'.format(client_socket.accept))
-        client_socket.close()
+            try:
+                recv_data = con.recv(bufsize)
+            except ConnectionResetError:
+                self.remove_conection(con, addr)
+                break
+            else:
+                if not recv_data:
+                    self.remove_conection(con, addr)
+                    break
+                else:
+                    self.do_message_dispatch(con, recv_data)
+
+        print('受信処理終了')
 
     def do_message_dispatch(self, client_scoket, message):
         pass
@@ -52,7 +74,7 @@ class MessageWorkder(TcpServer):
         return super().__init__(bindip, bindport)
 
     def do_message_dispatch(self, client_socket, message):
-        buffer = io.BytesIO(request)
+        buffer = io.BytesIO(message)
         t = IdentificationUnit()
         buffer.readinto(t)
 
@@ -71,7 +93,16 @@ class MessageWorkder(TcpServer):
 class App:
     _instance = None
     def __init__(self):
+        """アプリケーションの初期化処理"""
+
         print('Begin... Initalize application')
+        self._initalize_resource()
+
+        self.server = MessageWorkder('0.0.0.0', 10121)
+
+        # ヘルスタイマー登録
+        signal.signal(signal.SIGALRM, self._health_send_timer)
+        signal.setitimer(signal.ITIMER_REAL, 10, 10)
 
     @classmethod
     def get_instance(cls):
@@ -80,16 +111,21 @@ class App:
 
             return cls._instance
 
+    def _initalize_resource(self):
+        """何かｱﾌﾟﾘの初期化処理があれば記述"""
+        pass
+
 	# 実行部
     def run(self):
         """
             処理実行部
         """
-        server = MessageWorkder('0.0.0.0', 10121)
-        server.listen(5)
+        self.server.listen(5)
         while True:
-            print('start accept')
-            server.accept()
+            self.server.accept()
+
+    def _health_send_timer(self):
+        print('ヘルス送信')
 
 
-
+    
