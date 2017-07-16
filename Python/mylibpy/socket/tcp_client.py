ef3c9d70b904d5be767906179c9990a5261b990c
manyanxp <manyanxp.cat.com@gmail.com>
2017-07-16 10:25:24 +0900

    TCPクライアント実装、サーバー側を非同期接続、非同期受信化

--- Python/mylibpy/socket/tcp_client.py
+++ Python/mylibpy/socket/tcp_client.py
@@ -1,5 +1,66 @@
 # -*- utf-8 -*-
 import sys
+# -*- coding:utf-8 -*-
+import socket
+import asyncio
 
+class TcpClient():
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.__client = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #オブジェクトの作成をします
+        self._connected = None
+        self._discooneted = None
+
+    def connect(self):
+        """サーバーへ接続"""
+        ret = True
+        try:
+            self.__client.connect((self.host, self.port)) #これでサーバーに接続します
+            if self._connected is not None:
+                self._connected((self.host, self.port))
+        except ConnectionRefusedError:
+            ret = False
+        
+        return ret
+
+    def close(self):
+        self.close()
+
+    def sendto(self, data):
+        """パケット送信"""
+        self.__client.send(data) 
+
+    def connected_event(self, func):
+        """接続ｲﾍﾞﾝﾄ"""
+        self._connected = func
+
+    def disconnected_event(self, func):
+        """切断ｲﾍﾞﾝﾄ"""
+        self._discooneted = func
+
+    def recv(self):
+        try:
+            packet = self.__client.recv(4096)
+        except ConnectionResetError:
+            self.__client.close()
+        else:
+            if not recv_data:
+                self.remove_conection(con, addr)
+                if self._discooneted is not None:
+                    self._discooneted(addr)
+            else:
+                loop = asyncio.get_event_loop()
+                asyncio.ensure_future(self.recv_async(packet, loop))
+                loop.run_forever()
+
+    async def recv_async(self, recvdata):
+        pass
+ 
 if __name__ == '__main__':
-    pass
+    import time
+
+    client = TcpClient('127.0.0.1', 50001)
+    if client.connect() is not False:    
+        client.sendto("tetestset")
+        client.close()
--- Python/mylibpy/socket/tcp_server.py
+++ Python/mylibpy/socket/tcp_server.py
@@ -13,8 +13,11 @@ class TcpServer:
         """通信ソケットの作成"""
         self.clients = []
         self.bindip = "0.0.0.0"
-        self.bindport = 500000
-        self.lock = Lock()
+        self.bindport = 50000
+        self._lock = Lock()
+
+        self._connected = None
+        self._discooneted = None
 
     def bind(self, bindip, bindport):
         """ソケット作成"""
@@ -36,30 +39,45 @@ class TcpServer:
         client,addr = self.__server.accept()
         print('[*]Accepted connectoin from: %s:%d' % (addr[0],addr[1]))
 
-        with self.lock:
+        with self._lock:
             self.clients.append((client, addr))
             client_handler = threading.Thread(target=self.handle_client,args=(client,addr),)
             client_handler.daemon = True
             client_handler.start()
-
-    async def begin_accept_async(self):
-        """非同期による接続要求待ち"""
+            if self._connected is not None:
+                self._connected((client,addr))
+    
+    async def _begin_accept_async(self, loop):
+        """非同期による接続要求待ち（本体）"""
         while True:
             self.accept() 
-        return True
+
+    def begin_accept_async(self):
+        """非同期による接続要求待ち"""
+        loop = asyncio.get_event_loop()
+        asyncio.ensure_future(self._begin_accept_async(loop))
+        loop.run_forever()
 
     def remove_conection(self, con, addr):
         """クライアントと接続を切る"""
         print('切断{}'.format(addr))
-        with self.lock:
+        with self._lock:
             con.close()
             self.clients.remove((con, addr))
 
     def send_packet(self, messege):
         """クライアントへのパケット送信"""
-        with self.lock:
+        with self._lock:
             for c in self.clients:
                 c[0].sendto(message, c[1])
+
+    def connected_event(self, func):
+        """接続ｲﾍﾞﾝﾄ"""
+        self._connected = func
+
+    def disconnected_event(self, func):
+        """切断ｲﾍﾞﾝﾄ"""
+        self._discooneted = func
 
     def handle_client(self, con, addr):
         """クライアントハンドル"""
@@ -70,24 +88,39 @@ class TcpServer:
                 recv_data = con.recv(bufsize)
             except ConnectionResetError:
                 self.remove_conection(con, addr)
+                if self._discooneted is not None:
+                    self._discooneted(addr)
                 break
             else:
                 if not recv_data:
                     self.remove_conection(con, addr)
+                    if self._discooneted is not None:
+                        self._discooneted(addr)
                     break
                 else:
+                    loop = asyncio.get_event_loop()
                     asyncio.ensure_future(self.do_proc_packet_async(con, recv_data))
+                    loop.run_forever()
 
-        print('受信処理終了')
+        print('[*] 受信処理終了')
 
     async def do_proc_packet_async(self, client_scoket, message):
         """パケットの非同期処理テンプレート"""
         pass
 
+def disconnected(*arg):
+    print(arg[0])
 
 if __name__ == '__main__':
     server = TcpServer()
-    server.bind('0.0.0.0', 50000)
+    server.bind('0.0.0.0', 50001)
     server.listen(10)
-    asyncio.ensure_future(server.begin_accept_async())
+    #server.accept()
+    server.disconnected_event(disconnected)
+    server.begin_accept_async()
+
+
+    while True:
+        time.sleep(3)
+
 
